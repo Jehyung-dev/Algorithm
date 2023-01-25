b36706a76036442d29811de6ac687a9170d93d0e
Jehyung-dev <113454427+Jehyung-dev@users.noreply.github.com>
2023-01-25 23:33:07 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1412 ms, Memory: 76328 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,9 +1,12 @@
 # 수 정렬하기 2
+import sys
 
-N = int(input())    # N개의 정수를 받음
+ipt=sys.stdin.readline
+
+N = int(ipt())    # N개의 정수를 받음
 lst = []    # 입력을 받을 리스트를 만듦
 for i in range(N):  # N번의 반복으로 str형으로 수를 받음
-    lst.append(int(input()))
+    lst.append(int(ipt()))
 lst.sort()  # 오름차순 정렬
 for j in lst:   # 문자열을 잘라 출력
     print(j)
