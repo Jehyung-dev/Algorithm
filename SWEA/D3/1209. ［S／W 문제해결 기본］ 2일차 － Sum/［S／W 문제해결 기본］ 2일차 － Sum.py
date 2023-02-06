56b23d8278e4144ac64b64b256a77f2300b9a703
Jehyung-dev <113454427+Jehyung-dev@users.noreply.github.com>
2023-02-06 15:19:34 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 165 ms, Memory: 60,184 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -1,27 +1,33 @@
-# Sum
+# [S/W 문제해결 기본] 2일차 - Sum
 
-for i in range(10):
-    total_sum = []
-    tmp1 = 0
-    tmp2 = 0
-    T = int(input())
-    arr = [list(map(int, input().split())) for _ in range(100)]
+for _ in range(10):
+    t = int(input())
+    num_list = [list(map(int, input().split())) for _ in range(100)]
+    row_column_max = 0
 
-    for j in range(100):
-        for k in range(100):
-            tmp1 += arr[j][k]
-            tmp2 += arr[k][j]
-        total_sum.append(tmp1)
-        total_sum.append(tmp2)
-        tmp1 = 0
-        tmp2 = 0
+    for i in range(100):
+        row_sum = 0
+        column_sum = 0
+        for j in range(100):
+            row_sum += num_list[i][j]
+            column_sum += num_list[j][i]
+            if row_sum > column_sum and row_sum > row_column_max:
+                row_column_max = row_sum
+            elif column_sum > row_column_max:
+                row_column_max = column_sum
 
-    for l in range(100):
-        tmp1 += arr[l][l]
-        tmp2 += arr[l][99-l]
-    total_sum.append(tmp1)
-    total_sum.append(tmp2)
-    tmp1 = 0
-    tmp2 = 0
+    cross_max = 0
+    for m in range(100):
+        cross_sum1 = 0
+        cross_sum2 = 0
+        cross_sum1 += num_list[m][m]
+        cross_sum2 += num_list[m][99-m]
+        if cross_sum1 > cross_sum2 and cross_sum1 > cross_max:
+            cross_max = cross_sum1
+        elif cross_sum2 > cross_max:
+            cross_max = cross_sum2
 
-    print(f'#{T} {max(total_sum)}')
+    if row_column_max > cross_max:
+        print(f'#{t} {row_column_max}')
+    else:
+        print(f'#{t} {cross_max}')
