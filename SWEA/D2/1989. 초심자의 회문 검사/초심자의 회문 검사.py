61d8f4e8c508e657d492574fcd3107017698dd0b
Jehyung-dev <113454427+Jehyung-dev@users.noreply.github.com>
2023-02-08 12:10:08 +0900

    [D2] Title: 초심자의 회문 검사, Time: 92 ms, Memory: 42,704 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
@@ -3,11 +3,10 @@
 T = int(input())
 for t in range(T):
     word = list(input())
-    reversed_word = [0]*len(word)
-    for i in range(len(word)):
-        reversed_word[i] = word[len(word)-1-i]
 
-    if word == reversed_word:
-        print(f'#{t+1} 1')
+    for i in range(len(word)//2):
+        if word[i] != word[len(word)-1-i]:
+            print(f'#{t + 1} 0')
+            break
     else:
-        print(f'#{t + 1} 0')
+        print(f'#{t + 1} 1')
