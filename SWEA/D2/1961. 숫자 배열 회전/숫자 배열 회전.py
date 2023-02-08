a136c3feaebf887025100edfa25f7aff18783b2f
Jehyung-dev <113454427+Jehyung-dev@users.noreply.github.com>
2023-02-09 01:45:28 +0900

    [D2] Title: 숫자 배열 회전, Time: 108 ms, Memory: 45,832 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -3,62 +3,26 @@
 T = int(input())
 for i in range(T):
     N = int(input())
-    arr = [list(map(int,input().split())) for _ in range(N)]
+    arr = [list(map(int, input().split())) for _ in range(N)]
     arr_90 = [[] for _ in range(N)]
     arr_180 = [[] for _ in range(N)]
     arr_270 = [[] for _ in range(N)]
-    
+
     for j in range(0, N):
-        for k in range(-N+1, 1):
+        for k in range(-N + 1, 1):
             arr_90[j].append(arr[abs(k)][j])
 
     for j in range(0, N):
-        for k in range(-N+1, 1):
+        for k in range(-N + 1, 1):
             arr_180[j].append(arr_90[abs(k)][j])
 
     for j in range(0, N):
-        for k in range(-N+1, 1):
+        for k in range(-N + 1, 1):
             arr_270[j].append(arr_180[abs(k)][j])
-    
-    print(f'#{i+1}')
+
+    print(f'#{i + 1}')
     for l in range(N):
         tmp1 = arr_90[l]
         tmp2 = arr_180[l]
         tmp3 = arr_270[l]
-        print(''.join(map(str,tmp1)), ''.join(map(str,tmp2)), ''.join(map(str,tmp3)))
-
-# 2차원 리스트 join이 안됨 / join시 map으로 str형태를 만들어야함
-
-'''
-# 숫자 배열 회전
-
-T = int(input())
-for i in range(T):
-    N = int(input())
-    arr = [list(map(int,input().split())) for _ in range(N)]
-    arr_90 = [[] for _ in range(N)]
-    arr_180 = [[] for _ in range(N)]
-    arr_270 = [[] for _ in range(N)]
-    
-    for j in range(0, N):
-        for k in range(-N+1, 1):
-            arr_90[j].append(arr[abs(k)][j])
-
-    for j in range(0, N):
-        for k in range(-N+1, 1):
-            arr_180[j].append(arr_90[abs(k)][j])
-
-    for j in range(0, N):
-        for k in range(-N+1, 1):
-            arr_270[j].append(arr_180[abs(k)][j])
-    
-    print(f'#{i+1}')
-    for l in range(N):
-        tmp1 = arr_90[l]
-        tmp2 = arr_180[l]
-        tmp3 = arr_270[l]
-        print(*tmp1, sep='', end = ' ')
-        print(*tmp2, sep='', end = ' ')
-        print(*tmp3, sep='')
-# sep은 문자 사이의 추가, end는 출력 이후 방식
-'''
+        print(''.join(map(str, tmp1)), ''.join(map(str, tmp2)), ''.join(map(str, tmp3)))
