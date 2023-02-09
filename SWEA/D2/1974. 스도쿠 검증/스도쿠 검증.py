58095e3569d769dd4b82fd804cc22bfe5f294e09
Jehyung-dev <113454427+Jehyung-dev@users.noreply.github.com>
2023-02-09 17:51:23 +0900

    [D2] Title: 스도쿠 검증, Time: 98 ms, Memory: 47,164 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,19 +1,19 @@
 # 스도쿠 검증
- 
+  
 T = int(input())
- 
+  
 for i in range(T):
     arr = [list(map(int,input().split())) for _ in range(9)]
     chk_lst1 = [[] for _ in range(9)]
     chk_lst2 = [[] for _ in range(9)]
     chk_lst3 = [[] for _ in range(9)]
     err = 0
- 
+  
     for j in range(9):
         for k in range(9):
             chk_lst1[j].append(arr[k][j])
             chk_lst2[j].append(arr[j][k])
-     
+      
     for m in range(0,3):
         for n in range(0, 3):
             for o in range(0, 3):
@@ -26,11 +26,11 @@ for i in range(T):
         for n in range(0, 3):
             for o in range(0, 3):
                 chk_lst3[m].append(arr[o][n+6])                        
- 
+  
     for l in range(9):
         if len(chk_lst1[l]) != len(set(chk_lst1[l])) or len(chk_lst2[l]) != len(set(chk_lst2[l])) or len(chk_lst3[l]) != len(set(chk_lst3[l])):
             err += 1     
-     
+      
     if err == 0:
         print(f'#{i+1} 1')
     else:
