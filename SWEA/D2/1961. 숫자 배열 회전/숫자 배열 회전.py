ef84a21972049548271e4f2f5ca8614156ff6d07
Jehyung-dev <113454427+Jehyung-dev@users.noreply.github.com>
2023-02-09 17:52:38 +0900

    [D2] Title: 숫자 배열 회전, Time: 108 ms, Memory: 45,832 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,5 +1,5 @@
 # 숫자 배열 회전
-
+ 
 T = int(input())
 for i in range(T):
     N = int(input())
@@ -7,19 +7,19 @@ for i in range(T):
     arr_90 = [[] for _ in range(N)]
     arr_180 = [[] for _ in range(N)]
     arr_270 = [[] for _ in range(N)]
-
+ 
     for j in range(0, N):
         for k in range(-N + 1, 1):
             arr_90[j].append(arr[abs(k)][j])
-
+ 
     for j in range(0, N):
         for k in range(-N + 1, 1):
             arr_180[j].append(arr_90[abs(k)][j])
-
+ 
     for j in range(0, N):
         for k in range(-N + 1, 1):
             arr_270[j].append(arr_180[abs(k)][j])
-
+ 
     print(f'#{i + 1}')
     for l in range(N):
         tmp1 = arr_90[l]
