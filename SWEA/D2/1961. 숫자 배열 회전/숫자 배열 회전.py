1540fc3e69511ff8fc407331ed2e8c6a2dd84c73
Jehyung-dev <113454427+Jehyung-dev@users.noreply.github.com>
2023-02-10 10:19:30 +0900

    [D2] Title: 숫자 배열 회전, Time: 108 ms, Memory: 45,832 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,28 +1,32 @@
 # 숫자 배열 회전
- 
+
+def turn_90(arr, N):
+    turn_arr = [[0] * N for _ in range(N)]
+
+    for i in range(N - 1, -1, -1):
+        for j in range(0, N):
+            turn_arr[j][i] = arr[N - i - 1][j]
+    return turn_arr
+
+
 T = int(input())
-for i in range(T):
+for t in range(T):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
-    arr_90 = [[] for _ in range(N)]
-    arr_180 = [[] for _ in range(N)]
-    arr_270 = [[] for _ in range(N)]
- 
-    for j in range(0, N):
-        for k in range(-N + 1, 1):
-            arr_90[j].append(arr[abs(k)][j])
- 
-    for j in range(0, N):
-        for k in range(-N + 1, 1):
-            arr_180[j].append(arr_90[abs(k)][j])
- 
-    for j in range(0, N):
-        for k in range(-N + 1, 1):
-            arr_270[j].append(arr_180[abs(k)][j])
- 
-    print(f'#{i + 1}')
-    for l in range(N):
-        tmp1 = arr_90[l]
-        tmp2 = arr_180[l]
-        tmp3 = arr_270[l]
-        print(''.join(map(str, tmp1)), ''.join(map(str, tmp2)), ''.join(map(str, tmp3)))
+    arr_90 = [[0] * N for _ in range(N)]
+    arr_180 = [[0] * N for _ in range(N)]
+    arr_270 = [[0] * N for _ in range(N)]
+
+    for i in range(N - 1, -1, -1):
+        for j in range(0, N):
+            arr_90 = turn_90(arr, N)
+    for i in range(N - 1, -1, -1):
+        for j in range(0, N):
+            arr_180 = turn_90(arr_90, N)
+    for i in range(N - 1, -1, -1):
+        for j in range(0, N):
+            arr_270 = turn_90(arr_180, N)
+
+    print(f'#{t + 1}')
+    for idx in range(N):
+        print(''.join(map(str, arr_90[idx])), ''.join(map(str, arr_180[idx])), ''.join(map(str, arr_270[idx])))
