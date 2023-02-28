e67bc74f3c6162232c16953b0d28330be9cad0e8
Jehyung-dev <113454427+Jehyung-dev@users.noreply.github.com>
2023-02-28 11:14:22 +0900

    [Bronze II] Title: OX퀴즈, Time: 128 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -1,18 +1,16 @@
 # OX퀴즈
 
-cnt = 0
-sum = 0
-n = int(input())
+T = int(input())
 
-for i in range(n):
-    Quiz = list(input())
+for _ in range(T):
+    Quiz = input()
+    cnt = sum = 0
 
-    for j in range(len(Quiz)):
-        if Quiz[j] == 'O':
+    for i in range(len(Quiz)):
+        if Quiz[i] == 'O':
             cnt += 1
             sum += cnt
-        if Quiz[j] == 'X':
+        if Quiz[i] == 'X':
             cnt = 0
+
     print(sum)
-    sum = 0
-    cnt = 0
