fe5294a78d12eb6d2dbea4a504e88ab40bb6f2c7
Jehyung-dev <113454427+Jehyung-dev@users.noreply.github.com>
2023-02-28 10:58:49 +0900

    [Bronze II] Title: 나머지, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,8 +1,8 @@
 # 나머지
 
-import sys
+remain = set()
+for _ in range(10):
+    N = int(input())
+    remain.add(N%42)
 
-ipt = sys.stdin.readline
-number_list=[ipt().rstrip() for i in range(10)] # 한줄씩 입력되는 값을 리스트로 받음
-remainder_set=set(int(number_list[j])%42 for j in range(10))    # 중복을 제거하는 set사용
-print(len(remainder_set))   # 나머지 값의 갯수를 출력
+print(len(remain))
