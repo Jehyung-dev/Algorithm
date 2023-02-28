681ad192786173d7825fbb28c9091a95015e3143
Jehyung-dev <113454427+Jehyung-dev@users.noreply.github.com>
2023-02-28 15:22:49 +0900

    [D3] Title: 농작물 수확하기, Time: 217 ms, Memory: 63,036 KB -BaekjoonHub

---
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -0,0 +1,18 @@
+# # 농작물 수확하기
+
+T = int(input())
+for t in range(1, T + 1):
+    N = int(input())
+    arr = [list(input()) for _ in range(N)]
+
+    mid = (N - 1) // 2
+    up, down = mid - 1, mid + 1
+    i = 1
+    ans = sum(map(int, arr[mid]))
+
+    while up != -1:
+        ans += sum(map(int, arr[up][i:N - i])) + sum(map(int, arr[down][i:N - i]))
+        i += 1
+        down += 1
+        up -= 1
+    print(f'#{t} {ans}')
