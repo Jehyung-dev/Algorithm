580a251bf8371ad825893caa2a6fb5bad68e428f
Jehyung-dev <113454427+Jehyung-dev@users.noreply.github.com>
2023-02-28 10:37:56 +0900

    [Bronze II] Title: 벌집, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,15 +1,15 @@
 # 벌집
 
-import sys
-input = sys.stdin.readline
-output = sys.stdout.write
-N = int(input().rstrip())   # N 입력 받기
-num = 1 # 벌집 범위의 최대값
-cnt = 0 # 몇 칸 걸리는지 카운트
-if N==1:    # 1인경우는 그냥 1
-    output('1')
-else:   # 아닌 경우는 N이 작을 때까지 반복루프
-    while num < N:
-        num+=6*cnt
-        cnt+=1
-    output(f'{cnt}')
+N = int(input())
+cnt = 1
+
+if N == 1:
+    print(1)
+else:
+    start, end = 2, 7
+    while N < start or end < N:
+        start += 6 * cnt
+        end += 6 * (cnt + 1)
+        cnt += 1
+
+    print(cnt + 1)
