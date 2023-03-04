8c11b235bf6888174632486dcefa3bdc3c25bae5
Jehyung-dev <113454427+Jehyung-dev@users.noreply.github.com>
2023-03-04 17:53:48 +0900

    [D2] Title: 스도쿠 검증, Time: 98 ms, Memory: 47,164 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,39 +1,33 @@
 # 스도쿠 검증
 
-T = int(input())
+def Check_line():
+    for i in range(9):
+        temp = set()
+        if len(set(arr[i])) != 9:
+            return 0
+        for j in range(9):
+            temp.add(arr[j][i])
+        if len(temp) != 9:
+            return 0
+    return 1
 
-for i in range(T):
+
+def Check_box():
+    for i in range(3):
+        for j in range(3):
+            temp = set()
+            for k in range(3 * i, 3 * (i + 1)):
+                for l in range(3 * j, 3 * (j + 1)):
+                    temp.add(arr[k][l])
+            if len(temp) != 9:
+                return 0
+    return 1
+
+
+for t in range(int(input())):
     arr = [list(map(int, input().split())) for _ in range(9)]
-    chk_lst1 = [[] for _ in range(9)]
-    chk_lst2 = [[] for _ in range(9)]
-    chk_lst3 = [[] for _ in range(9)]
-    err = 0
 
-    for j in range(9):
-        for k in range(9):
-            chk_lst1[j].append(arr[k][j])
-            chk_lst2[j].append(arr[j][k])
-
-    for m in range(0, 3):
-        for n in range(0, 3):
-            for o in range(0, 3):
-                chk_lst3[m].append(arr[o + m * 3][n])
-    for m in range(3, 6):
-        for n in range(0, 3):
-            for o in range(0, 3):
-                chk_lst3[m].append(arr[o][n + 3])
-    for m in range(6, 9):
-        for n in range(0, 3):
-            for o in range(0, 3):
-                chk_lst3[m].append(arr[o][n + 6])
-
-    for l in range(9):
-        if len(chk_lst1[l]) != len(set(chk_lst1[l])) or len(chk_lst2[l]) != len(set(chk_lst2[l])) or len(
-                chk_lst3[l]) != len(set(chk_lst3[l])):
-            err += 1
-            break
-
-    if err == 0:
-        print(f'#{i + 1} 1')
+    if Check_line() and Check_box():
+        print(f'#{t + 1} 1')
     else:
-        print(f'#{i + 1} 0')
+        print(f'#{t + 1} 0')
