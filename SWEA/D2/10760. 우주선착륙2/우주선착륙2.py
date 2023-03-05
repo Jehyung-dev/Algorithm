ba3cf6c4704492acd09f7949b9bd14f11a4571f0
Jehyung-dev <113454427+Jehyung-dev@users.noreply.github.com>
2023-03-05 15:19:05 +0900

    [D2] Title: 우주선착륙2, Time: 223 ms, Memory: 63,048 KB -BaekjoonHub

--- SWEA/D2/10760. 우주선착륙2/우주선착륙2.py
+++ SWEA/D2/10760. 우주선착륙2/우주선착륙2.py
@@ -1,29 +1,23 @@
 # 우주선착륙2
 
-def Check(r,c):
-    global result
-    cnt = 0
-    for di in range(8):
-        nr = r + dr[di]
-        nc = c + dc[di]
-        if nr < 0 or nc < 0 or nr >= N or nc >= M:
-            continue
-        else:
-            if arr[nr][nc] < arr[r][c]:
-                cnt+=1
-        if cnt >= 4:
-            result += 1
-            break
+def is_Site():
+    ans = 0
+    for r in range(N):
+        for c in range(M):
+            cnt = 0
+            for di in range(8):
+                nr, nc = r + dr[di], c + dc[di]
+                if 0 <= nr < N and 0 <= nc < M and arr[r][c] > arr[nr][nc]:
+                    cnt += 1
+            if cnt >= 4:
+                ans += 1
+    return ans
 
 
-dr = [-1,-1,0,1,1,1,0,-1]
-dc = [0,1,1,1,0,-1,-1,-1]
-T = int(input())
-for t in range(T):
+dr = [-1, -1, 0, 1, 1, 1, 0, -1]
+dc = [0, 1, 1, 1, 0, -1, -1, -1]
+for t in range(int(input())):
     N, M = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
-    result = 0
-    for i in range(N):
-        for j in range(M):
-            Check(i,j)
-    print(f'#{t+1} {result}')
+
+    print(f'#{t + 1} {is_Site()}')
